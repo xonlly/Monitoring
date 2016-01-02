34b7a802540fae895beaa1b381ad30c99c53afdb
Xonlly <ariege.creation@gmail.com>
2016-01-03 00:14:59 +0100

    Clean and upgrade 2.0.0
    Première version stable

--- client.js
+++
@@ -1,9 +0,0 @@
-"use strict"
-
-const Client = require('./core/Client');
-
-const MyServer = new Client({
-  key : 'A45DE87Daez89D',
-  master : '127.0.0.1',
-  ping : 1000 // Ping toutes les 10 secondes.
-})
--- server.js
+++
@@ -1,7 +0,0 @@
-"use strict"
-
-const Server = require('./core/Server');
-new Server({
-  key : 'A45DE87Daez89D',
-  master : '127.0.0.1',
-})
