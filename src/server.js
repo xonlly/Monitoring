5102b3e64d19a03d3ddf16978bb0a0916a656e82
Xonlly <ariege.creation@gmail.com>
2016-01-03 01:20:54 +0100

    Remove option

--- src/server.js
+++ src/server.js
@@ -2,7 +2,6 @@ const Server = require('./service/server.js')
 const Args = require('./utils/args')()
 
 new Server({
-  serverName : "Devteck Monitoring",
   key : Args.key || "456DAde486qD684de6",
   port : Args.port || 8156,
 })
